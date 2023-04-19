e0b43961972ec380638bd2e11ec196cc5a5e6bdb
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-19 20:37:38 +0200

    Add initial UI

--- task.py
+++ task.py
@@ -1,1 +1,24 @@
-print("Learning progress tracker")
+import sys
+
+
+class TrackerApplication:
+    def __init__(self):
+        pass
+
+    def execute(self):
+        print("Learning progress tracker")
+        while (command := input().strip()) != "exit":
+            if not command:
+                print("No input")
+            else:
+                print("Unknown command!")
+        print("Bye!")
+
+
+def main():
+    tracker = TrackerApplication()
+    tracker.execute()
+
+
+if __name__ == "__main__":
+    main()
