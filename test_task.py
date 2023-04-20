0789522ce6aa6a81ffd24514a4ed37c5c7714f76
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-20 15:24:25 +0200

    Add tests

---
+++ test_task.py
@@ -0,0 +1,43 @@
+# fmt: off
+import pytest
+from .task import TrackerApplication
+
+
+class TestTracker:
+    @pytest.fixture(autouse=True)
+    def setup_tracker(self):
+        self.tracker = TrackerApplication()
+
+    def test_correct_credentials(self):
+        assert self.tracker.validate_credentials(["John", "Middle", "Smith", "email@dot.com"]) is not None
+        assert self.tracker.validate_credentials(["John", "Smith", "email@dot.com"]) is not None
+
+    def test_incorrect_number_of_credential_fields(self):
+        assert self.tracker.validate_credentials([]) is None
+        assert self.tracker.validate_credentials(["John"]) is None
+        assert self.tracker.validate_credentials(["John", "Smith"]) is None
+        assert self.tracker.validate_credentials(["John", "email@example.com"]) is None
+
+    def test_valid_email(self):
+        assert self.tracker.validate_email("email@example.com")
+        assert self.tracker.validate_email("email@example.co.uk")
+        assert self.tracker.validate_email("name.surname@example.com")
+
+    def test_invalid_email(self):
+        assert not self.tracker.validate_email("name@surname@example.com")
+        assert not self.tracker.validate_email("email@example")
+        assert not self.tracker.validate_email("name.surname@.com")
+        assert not self.tracker.validate_email("@example.com")
+
+    def test_valid_name(self):
+        assert self.tracker.validate_name("John")
+        assert self.tracker.validate_name("Jean-Claude")
+        assert self.tracker.validate_name("O'Neill")
+        assert self.tracker.validate_name("Louis XVI")
+
+    def test_invalid_name(self):
+        assert not self.tracker.validate_name("John'")
+        assert not self.tracker.validate_name("Jean -Claude")
+        assert not self.tracker.validate_name("O-'Neill")
+        assert not self.tracker.validate_name("John S")
+        assert not self.tracker.validate_name("Stanisław Oğuz")
