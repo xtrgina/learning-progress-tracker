ceacce1243dde9b67c9d42be47dbe5f8633c1fef
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-21 17:48:33 +0200

    Implement "statistics" command

--- task.py
+++ task.py
@@ -2,6 +2,7 @@ import sys
 import re
 from dataclasses import dataclass, field
 from itertools import count
+from statistics import fmean
 
 
 @dataclass
@@ -15,26 +16,47 @@ class Student:
 @dataclass
 class Course:
     name: str
+    pass_requirement: int
     student_records: dict[int, int] = field(default_factory=dict)
+    submissions: list[int] = field(default_factory=list)
 
     def update_student_record(self, student_id: int, points: int) -> None:
+        if points == 0:
+            return
         if student_id in self.student_records:
             self.student_records[student_id] += points
         else:
             self.student_records[student_id] = points
+        self.submissions.append(points)
+
+    def get_student_data(self):
+        student_data = []
+        for student_id, points in self.student_records.items():
+            completed_percentage = f"{(points/self.pass_requirement)*100:.1f}%"
+            student_data.append((student_id, points, completed_percentage))
+        return student_data
+
+    def average_points_per_submission(self):
+        return fmean(self.submissions)
+
+    def total_number_of_submissions(self):
+        return len(self.submissions)
 
     def get_points_by_student_id(self, student_id: int) -> int:
         return self.student_records[student_id]
+
+    def get_enrolled_count(self):
+        return len([p for p in self.student_records.values() if p > 0])
 
 
 class TrackerApplication:
     def __init__(self):
         self.students = {}
         self.courses = {
-            "Python": Course("Python"),
-            "DSA": Course("DSA"),
-            "Databases": Course("Databases"),
-            "Flask": Course("Flask"),
+            "python": Course("Python", 600),
+            "dsa": Course("DSA", 400),
+            "databases": Course("Databases", 480),
+            "flask": Course("Flask", 550),
         }
 
     def execute(self):
@@ -54,6 +76,8 @@ class TrackerApplication:
                     self.find_student()
                 case "list":
                     self.list_students()
+                case "statistics":
+                    self.statistics()
                 case _:
                     print("Unknown command!")
         print("Bye!")
@@ -180,6 +204,32 @@ class TrackerApplication:
                 points = course.get_points_by_student_id(student_id)
                 course_info.append(f"{course_name}={points}")
             print(f"{student_id} points: {'; '.join(course_info)}")
+
+    def statistics(self):
+        print("Type the name of a course to see details or 'back' to quit:")
+        print("Most popular: n/a")
+        print("Least popular: n/a")
+        print("Highest activity: n/a")
+        print("Lowest activity: n/a")
+        print("Easiest course: n/a")
+        print("Hardest course: n/a")
+        while (course_name := input().lower().strip()) != "back":
+            if course_name not in self.courses:
+                print("Unknown course")
+                continue
+            self.display_course_statistics(course_name)
+
+    def display_course_statistics(self, course_name):
+        course = self.courses[course_name]
+        print(course.name)
+        print(f"{'id':<8}{'points':<10}completed")
+        student_statistics = sorted(
+            course.get_student_data(),
+            key=lambda data: (data[1], -data[0]),
+            reverse=True,
+        )
+        for student_id, points, completed in student_statistics:
+            print(f"{student_id:<8d}{points:<10d}{completed}")
 
 
 if __name__ == "__main__":
