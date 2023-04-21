c9e9d7873adcf49ab6361477273ad769fa53a004
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-21 13:15:46 +0200

    Implement "add points" command

--- task.py
+++ task.py
@@ -12,9 +12,27 @@ class Student:
     student_id: int = field(default_factory=count(10000).__next__)
 
 
+@dataclass
+class Course:
+    name: str
+    student_records: dict[int, int] = field(default_factory=dict)
+
+    def update_student_record(self, student_id: int, points: int) -> None:
+        if student_id in self.student_records:
+            self.student_records[student_id] += points
+        else:
+            self.student_records[student_id] = points
+
+
 class TrackerApplication:
     def __init__(self):
         self.students = {}
+        self.courses = {
+            "Python": Course("Python"),
+            "DSA": Course("DSA"),
+            "Databases": Course("Databases"),
+            "Flask": Course("Flask"),
+        }
 
     def execute(self):
         print("Learning progress tracker")
@@ -25,6 +43,8 @@ class TrackerApplication:
             match command:
                 case "add students":
                     self.add_students()
+                case "add points":
+                    self.add_points()
                 case "back":
                     print("Enter 'exit' to exit the program")
                 case "list":
@@ -42,9 +62,6 @@ class TrackerApplication:
             if credentials is None:
                 continue
             first_name, last_name, email = credentials
-            if self.is_email_taken(email):
-                print("This email is already taken")
-                continue
             student = Student(first_name, last_name, email)
             self.students[student.student_id] = student
             print("The student has been added")
@@ -54,7 +71,7 @@ class TrackerApplication:
         )
 
     def is_email_taken(self, email):
-        for student in self.students:
+        for student in self.students.values():
             if student.email == email:
                 return True
         return False
@@ -74,6 +91,9 @@ class TrackerApplication:
             return None
         if not self.validate_email(email):
             print("Incorrect email")
+            return None
+        if self.is_email_taken(email):
+            print("This email is already taken")
             return None
         return first_name, last_name, email
 
@@ -98,6 +118,45 @@ class TrackerApplication:
         print("Students:")
         for student_id in self.students:
             print(student_id)
+
+    def add_points(self):
+        print("Enter an id and points or 'back' to return")
+        while (user_input := input()) != "back":
+            validated_input = self.validate_points_string(user_input)
+            if validated_input is None:
+                continue
+            student_id, points = validated_input
+            for index, course_name in enumerate(self.courses):
+                self.update_course_record(course_name, student_id, points[index])
+            print("Points updated")
+
+    def validate_points_string(self, user_input):
+        input_fields = user_input.split()
+        if len(input_fields) != 5:
+            print("Incorrect points format")
+            return None
+        try:
+            student_id = int(input_fields[0])
+        except ValueError:
+            print(f"No student is found for id={input_fields[0]}")
+            return None
+        try:
+            points = [int(x) for x in input_fields[1:5]]
+        except ValueError:
+            print("Incorrect points format")
+            return None
+        if student_id not in self.students:
+            print(f"No student is found for id={student_id}")
+            return None
+        for number in points:
+            if number < 0:
+                print("Incorrect points format")
+                return None
+        return student_id, points
+
+    def update_course_record(self, course_name, student_id, points):
+        course = self.courses[course_name]
+        course.update_student_record(student_id, points)
 
 
 if __name__ == "__main__":
--- test_task.py
+++ test_task.py
@@ -1,6 +1,6 @@
 # fmt: off
 import pytest
-from .task import TrackerApplication, Student
+from .task import TrackerApplication, Student, Course
 
 
 class TestTracker:
@@ -11,6 +11,7 @@ class TestTracker:
     def test_correct_credentials(self):
         assert self.tracker.validate_credentials(["John", "Middle", "Smith", "email@dot.com"]) is not None
         assert self.tracker.validate_credentials(["John", "Smith", "email@dot.com"]) is not None
+        assert self.tracker.validate_credentials(["Jane", "Doe", "j@doe.com"]) is not None
 
     def test_incorrect_number_of_credential_fields(self):
         assert self.tracker.validate_credentials([]) is None
@@ -42,6 +43,34 @@ class TestTracker:
         assert not self.tracker.validate_name("John S")
         assert not self.tracker.validate_name("Stanisław Oğuz")
 
+    def test_invalid_points_string(self):
+        tracker = TrackerApplication()
+        student = Student("John", "Doe", "john@doe.com")
+        tracker.students[student.student_id] = student
+        assert not tracker.validate_points_string("100 1 2 3 4")
+        assert not tracker.validate_points_string("abcd 1 2 3 4")
+        assert not tracker.validate_points_string("")
+        assert not tracker.validate_points_string("10000 1 2 3")
+        assert not tracker.validate_points_string("10000 1 2 3 4 5")
+        assert not tracker.validate_points_string("10000 -1 2 3 4")
+        assert not tracker.validate_points_string("10000 a b c d")
+
+    def test_valid_points_string(self):
+        tracker = TrackerApplication()
+        student1 = Student("John", "Doe", "john@doe.com")
+        student2 = Student("Jane", "Spark", "jspark@gmail.com")
+        tracker.students[student1.student_id] = student1
+        tracker.students[student2.student_id] = student2
+        assert tracker.validate_points_string(f"{student1.student_id} 1 2 3 4")
+        assert tracker.validate_points_string(f"{student1.student_id} 0 0 0 0")
+        assert tracker.validate_points_string(f"{student2.student_id} 1 2 3 4")
+
+    def test_non_unique_email(self):
+        tracker = TrackerApplication()
+        student1 = Student("John", "Doe", "j@doe.com")
+        tracker.students[student1.student_id] = student1
+        assert tracker.is_email_taken("j@doe.com")
+        assert tracker.validate_credentials(["Jane", "Doe", "j@doe.com"]) is None
 class TestStudent():
     def test_student_id_increment(self):
         s1 = Student("John", "Doe", "john@doe.com")
